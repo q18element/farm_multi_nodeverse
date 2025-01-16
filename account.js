6b76ebb015142296902c99038b315cca959837ae
TruongTrReal <124138953+TruongTrReal@users.noreply.github.com>
2025-01-16 18:26:59 +0700

    ok

---
+++ account.js
@@ -0,0 +1,13 @@
+const Farm = require('./node_handler/automation');
+const { processAccountsAndProxies } = require('./proxy_handler/assign_proxy');
+
+const accountFilePath = "./config/accounts.txt";
+const outputFilePath = "./config/account_with_proxy.json";
+
+
+async function main () {
+  const assignedAccounts = await processAccountsAndProxies(accountFilePath, outputFilePath);
+
+}
+
+main()
--- get.js
+++ get.js
@@ -1,16 +1,9 @@
 const Farm = require('./node_handler/automation');
 const { processProxies } = require('./proxy_handler/main');
-const { processAccountsAndProxies } = require('./proxy_handler/assign_proxy');
-
-const accountFilePath = "./config/accounts.txt";
-const outputFilePath = "./config/account_with_proxy.json";
-
 
 async function main () {
   // Proxy processing
   await processProxies()
-  // Account processing
-  const assignedAccounts = await processAccountsAndProxies(accountFilePath, outputFilePath);
 
 }
 
